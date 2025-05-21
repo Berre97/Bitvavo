b8e3c56f6de8dafe7ebbe0af6c67a0e8b7b452ca
Berre97 <bertremon@hotmail.com>
2025-05-21 18:57:56 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -306,7 +306,7 @@ def main(bot):
                 data = json.load(f)
                 for order in data:
                     for i in open_orders:
-                        if order["market"] == market and i["orderId"] == order["Id"] and current_price >= float(order["price"]) * 1+(take_profit_percentage/100):
+                        if order["market"] == market and i["orderId"] == order["Id"::
                             print('TRUE')
                             print(current_price / order['price'])
 
