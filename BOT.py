f0fffc9606bb2234b230a50d1f0f0cac412a7465
Berre97 <bertremon@hotmail.com>
2025-05-21 22:36:41 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -306,7 +306,7 @@ def main(bot):
                 data = json.load(f)
                 for order in data:
                     for i in open_orders:
-                        if order["market"] == market and i["orderId"] == order["Id"] and float(current_price) >= float(order['price']) * take_profit_percentage:
+                        if order["market"] == market and i["orderId"] == order["Id"] and float(current_price) >= float(order['price']) * 1+(take_profit_percentage/100):
                             bitvavo.cancelOrder(market, order["Id"])
                             bitvavo.placeOrder(market, "sell", "market", {'amount': order["amount"]})
                             print(float(current_price) / float(order['price']))
