6749d473e7b53907f2c82553686b808b871a1813
Berre97 <bertremon@hotmail.com>
2025-05-23 11:29:38 +0200

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -37,7 +37,7 @@ class apibot():
         self._index = 0
         self._chat_id = -4717875969
         self._msg_id = None
-        self._file_path = os.getenv("FILE_PATH_BUYORDER")
+        self._file_path = os.getenv("FILE_PATH_BUYORDERS")
 
 
     async def timeout_sessie(self, chat_id):
