6b59e3faaa3961f585e08d44100c0faedcc8f594
Berre97 <bertremon@hotmail.com>
2025-06-25 20:20:23 +0200

    Update summary.py

--- summary.py
+++ summary.py
@@ -8,6 +8,7 @@ past_week = now-timedelta(days=7)
 buyorders = os.getenv("FILE_PATH_BUYORDERS")
 api_keys = json.loads(os.getenv('API_KEYS'))
 weekly_profit = []
+total_profit = []
 token = api_keys['token']
 chat_id = api_keys["chat_id"]
 bot = Bot(token=token)
@@ -15,12 +16,19 @@ bot = Bot(token=token)
 with open(buyorders, "r") as f:
     data = json.load(f)
     for order in data:
-        if "sold" in data and order["date"] >= past_week:
-            weekly_profit.append(order['eur_profit'])
-
+        if "sold" in data:
+            if order["date"] >= past_week
+                weekly_profit.append(order['eur_profit'])
+            total_profit.append(order['eur_profit']) 
+        
+if total_profit:
+    bot.send_message(f"Totaal gerealiseerde winst: €{round(sum(total_profit),2)}")
+    
 if weekly_profit:
     total_weekly_profit = round(sum(weekly_profit),2)
     avg_weekly_profit_per_trade = round(total_weekly_profit / len(weekly_profit),2)
     bot.send_message(f"Weekoverzicht\n"
                      f"Winst: €{total_weekly_profit}\n"
                      f"Gemmidelde winst per trade: €{avg_weekly_profit_per_trade}")
+
+
